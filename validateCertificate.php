6c9b0d21ebd770870d5539c93f40f3c42aaf212d
Rafael Guterres Jeffman <rafasgj@gmail.com>
2017-06-15 16:54:49 -0300

    Adicionado processo de verificacao de certificados.

---
+++ validateCertificate.php
@@ -0,0 +1,56 @@
+<?php
+
+function error($msg) {
+    #global $event_data, $fingerprint;
+    http_response_code(204);
+    #echo "# ".$msg." # ".$event_data." # ".$fingerprint." #";
+    exit();
+}
+
+if (isset($_GET['event_info'])) {
+    $fingerprint = $_GET['code'];
+    $event_data = $_GET['event_info'];
+} else {
+    if (count($argv) != 3) {
+        echo "\nusage:\n\t${argv[0]} <fingerprint> <event_info>\n\n";
+        exit();
+    }
+    $fingerprint = $argv[1];
+    $event_data = $argv[2];
+}
+
+$event_info = explode(":",$event_data);
+$DATA = $event_info[0];
+$CODENAME = $event_info[1];
+
+$filename = 'data/'.$DATA.'-'.$CODENAME.'.json';
+
+$json = file_get_contents($filename);
+if (! isset($json)) {
+    //TODO: render error page. "Não foi possível encontrar os dados
+    // do evento."
+    error("Cannot read JSON file.");
+}
+$data = json_decode($json,true);
+
+$INSTITUICAO = $data['instituicao'];
+$CIDADE = $data['cidade'];
+$HORAS = $data['horas'];
+$DATA = $data['data'];
+
+foreach ($data['participantes'] as $_ => $participante) {
+    if ($fingerprint == $participante['fingerprint']) {
+        $EMAIL = $participante['email'];
+        $FULANO = $participante['nome'];
+        $data = array('nome'=>$FULANO, 'data'=>$DATA, 'cidade'=>$CIDADE,
+                      'horas'=>$HORAS, 'fingerprint' => $fingerprint);
+        $json = json_encode($data);
+        http_response_code(200);
+        echo $json;
+        exit();
+    }
+}
+
+error("Cannot find user.");
+
+?>
