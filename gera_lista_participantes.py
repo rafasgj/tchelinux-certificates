9127f995add4639d0295d52f8cf6937a392bab8e
Rafael Guterres Jeffman <rafasgj@gmail.com>
2019-08-26 18:10:29 -0300

    Atualizada interface do gerador de certificados.
    Com esta alteração, é possível configurar todos os dados do evento
    a partir do JSON, e utilizar multiplos arquivos CSV para a geração
    dos dados dos participantes, permitindo a geração dos certificados
    de forma fracionada quando necessário.

--- gera_lista_participantes.py
+++ gera_lista_participantes.py
@@ -24,58 +24,77 @@ def fp(nome, email, local, ano, mes, dia):
     return hashlib.md5(data.encode('utf-8')).hexdigest()
 
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 2:
+    #    gera_lista_participantes.py config.json csv_participantes
     print("""
     usage:
-        gera_lista_participantes.py config.json csv_participantes
-            config.json:
-                horas: número de horas do evento.
-                instituicao: nome da instituicao.
-                cidade: nome da cidade em que ocorreu o evento.
-                organizacao: numero de horas dos organizadores (opcional).
+        gera_lista_participantes.py <arquivo_de_configuracao_do_evento>
+
+        O arquivo de configuração do evento é um arquivo JSON contendo:
+            horas: número de horas do evento.
+            instituicao: nome da instituicao.
+            cidade: nome da cidade em que ocorreu o evento.
+            cname: CNAME utilizado no site do evento.
+            organizacao: numero de horas dos organizadores (opcional).
+            data: data do evento no formato YYYY-MM-aa
+            files: lista de arquivos CSV do evento.
+
+        Os arquivos CSV de participantes devem ter o formato:
+            nome, email, participou (vazio para não), papel, palestra, ...
+
+        Observações:
+        * O papel pode ser 'organizador', 'palestrante' ou 'participante'.
+        * Caso o participante também seja palestrante, são necessárias duas
+          entradas no arquivo, uma para cada papel.
+        * Participantes não podem ser Organizadores.
+        * Organizador tem prioridade sobre participante.
+        * As palestras são opcionais e seu número é variável.
     """)
     sys.exit(1)
 
 with open(sys.argv[1], "r") as f:
     config = json.load(f)
-filename = sys.argv[2]
+# filename = sys.argv[2]
 horas = config.get('horas', 5)
 instituicao = config['instituicao']
 cidade = config['cidade']
 horas_organizacao = config.get('horas_organizacao', horas)
+filelist = config['files']
+local = config['cname']
+ano, mes, dia = config['data'].split('-')
+# ano, mes, dia, local, *_ = filename.split('-')
+# ano = ano.split('/')[-1]
+# local = local.split('.')[0]
 
-ano, mes, dia, local, *_ = filename.split('-')
-ano = ano.split('/')[-1]
-local = local.split('.')[0]
-
-with open(filename) as csvfile:
-    reader = csv.reader(csvfile)
-    lista = [{'nome': row[0],
-              'email': row[1],
-              'fingerprint': fp(row[0], row[1], local, ano, mes, dia),
-              'role': row[3] if len(row) > 3 else "participante",
-              'palestras': row[4:] if len(row) > 4 and
-              row[3] == 'palestrante' else []}
-             for row in reader
-             if row[2] != '' and row[1].lower()[:4] != "nome"]
-    participantes = {}
-    for x in lista:
-        p = participantes.get(x['email'], {'nome': x['nome'],
-                                           'email': x['email'],
-                                           'fingerprint': x['fingerprint'],
-                                           'roles': [],
-                                           'palestras': set()
-                                           })
-        roles = p['roles']
-        if x['role'] not in roles:
-            p['roles'].append(x['role'])
-        if x['role'] == 'palestrante':
-            [p['palestras'].add(a.strip()) for a in x['palestras']
-             if len(a.strip()) > 0]
-        participantes[p['email']] = p
-    for p in participantes.keys():
-        palestras = list(participantes[p]['palestras'])
-        participantes[p]['palestras'] = palestras
+participantes = {}
+for filename in filelist:
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        lista = [{'nome': row[0],
+                  'email': row[1],
+                  'fingerprint': fp(row[0], row[1], local, ano, mes, dia),
+                  'role': row[3] if len(row) > 3 else "participante",
+                  'palestras': row[4:] if len(row) > 4 and
+                  row[3] == 'palestrante' else []}
+                 for row in reader
+                 if row[2] != '' and row[1].lower()[:4] != "nome"]
+        for x in lista:
+            p = participantes.get(x['email'], {'nome': x['nome'],
+                                               'email': x['email'],
+                                               'fingerprint': x['fingerprint'],
+                                               'roles': [],
+                                               'palestras': set()
+                                               })
+            roles = p['roles']
+            if x['role'] not in roles:
+                p['roles'].append(x['role'])
+            if x['role'] == 'palestrante':
+                [p['palestras'].add(a.strip()) for a in x['palestras']
+                 if len(a.strip()) > 0]
+            participantes[p['email']] = p
+        for p in participantes.keys():
+            palestras = list(participantes[p]['palestras'])
+            participantes[p]['palestras'] = palestras
 
 evento = {"horas": horas, "instituicao": instituicao,
           "horas_organizacao": horas_organizacao,
